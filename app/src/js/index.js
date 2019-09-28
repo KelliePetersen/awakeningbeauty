617c023c41ee6da14142ea3441f4dd22e5600155
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-09-28 03:01:28 -0400

    add animated modal

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,9 +1,11 @@
 import WidescreenMenu from './modules/WidescreenMenu';
 import BackToTop from './modules/BackToTop';
+import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 const widescreenMenu = new WidescreenMenu();
 const backToTop = new BackToTop();
+const mobileMenu = new MobileMenu();
 const revealOnScroll = new RevealOnScroll();
 
 // revealOnScroll.sr.reveal(".heading", { 
