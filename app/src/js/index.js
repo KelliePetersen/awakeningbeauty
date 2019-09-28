5cebcd9fd40da3a673c85fd2ad57e1c1d491674a
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-09-28 19:18:08 -0400

    remove scroll reveal

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,16 +1,7 @@
 import WidescreenMenu from './modules/WidescreenMenu';
 import BackToTop from './modules/BackToTop';
 import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
 
 const widescreenMenu = new WidescreenMenu();
 const backToTop = new BackToTop();
 const mobileMenu = new MobileMenu();
-const revealOnScroll = new RevealOnScroll();
-
-// revealOnScroll.sr.reveal(".heading", { 
-//   distance: "10px",
-//   origin: "right",
-//   delay: 600,
-//   viewOffset: { bottom: -300 }
-// });
